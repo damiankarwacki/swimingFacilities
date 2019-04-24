b148edaa4aeb5994b6d4d9b862674205b66d3da1
Damian Karwacki <damian.karwacki@mindmade.pl>
2019-04-24 18:46:25 +0200

    initial commit

---
+++ src/main/java/com/sport/SportFacilities/SportFacilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.sport.SportFacilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportFacilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportFacilitiesApplication.class, args);
+	}
+
+}
